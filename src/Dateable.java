0b46896bbba4b0ff621525c24ac60267fe24a511
Nicole Gao <gao.lieke@gmail.com>
2024-08-23 12:45:49 +0800

    Lab 1

--- src/Dateable.java
+++ src/Dateable.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Dateable {
 
     public static void main(String[] args) {
-        
+        System.out.println("Enter your age");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        int minage = age/2 + 7;
+        System.out.println(age + "-year olds should date somebody who is at least " + minage);
     }
 }
--- src/Pages.java
+++ src/Pages.java
@@ -1,6 +1,12 @@
+import java.util.Scanner;
+
 public class Pages {
 
     public static void main(String[] args) {
-
+        System.out.println("Enter your age");
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
+        int pages = 100-age;
+        System.out.println(age + "-year olds should read at least " + pages + " pages before giving up on a book.");
     }
 }
